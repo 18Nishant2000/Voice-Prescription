7e0335067ed857719748b6c515abfbe5b4923d7c
Nishant Bansal <bansal.nishant18052000@gmail.com>
2020-02-01 15:25:53 +0530

    text to pdf almost done

--- app/src/main/java/com/example/e_prescription/MainActivity.java
+++ app/src/main/java/com/example/e_prescription/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.e_prescription;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
 import android.speech.RecognizerIntent;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -13,6 +20,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -20,21 +29,24 @@ public class MainActivity extends AppCompatActivity {
 
     Button mic,send;
     TextView out;
-    EditText pno;
-    String message,mobile;
+    EditText name,symptoms,diagnosis,prescription,advice;
+    //String message,mobile;
+    int c=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+      name=findViewById(R.id.name);
+      symptoms=findViewById(R.id.symptoms);
+      diagnosis=findViewById(R.id.diagnosis);
+      prescription=findViewById(R.id.prescription);
+      advice=findViewById(R.id.advice);
       mic=findViewById(R.id.mic_btn);
-      out=findViewById(R.id.out);
-      pno=findViewById(R.id.pno);
-      send=findViewById(R.id.send);
 
-      message=out.getText().toString();
-      mobile=pno.getText().toString().trim();
+      //message=out.getText().toString();
 
       mic.setOnClickListener(new View.OnClickListener() {
           @Override
@@ -51,22 +63,29 @@ public class MainActivity extends AppCompatActivity {
           }
       });
 
-      send.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
 
-              try {
-                  SmsManager smsManager=SmsManager.getDefault();
-                  smsManager.sendTextMessage(mobile,null,message,null,null);
-                  Toast.makeText(MainActivity.this, "MESSAGE SENT", Toast.LENGTH_SHORT).show();
-              }
-              catch (Exception e){
-                  Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
-              }
 
-          }
-      });
 
+    }
+
+    public void createpdf(View view){
+        PdfDocument pd=new PdfDocument();
+        PdfDocument.PageInfo mPageInfo=new PdfDocument.PageInfo.Builder(300,600,1).create();
+        PdfDocument.Page page=pd.startPage(mPageInfo);
+        Paint mp=new Paint();
+        page.getCanvas().drawText(name.getText().toString(),10,25,mp);
+
+        pd.finishPage(page);
+        String myFilePath= Environment.getExternalStorageDirectory().getPath()+"/pdffile.pdf";
+        File myfile=new File(myFilePath);
+        try {
+            pd.writeTo(new FileOutputStream(myfile));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            name.setText("Error");
+        }
+        pd.close();
     }
 
     @Override
@@ -76,7 +95,17 @@ public class MainActivity extends AppCompatActivity {
             case 1000:
                 if(resultCode==RESULT_OK && null!=data){
                     ArrayList<String> result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    out.setText(result.get(0));
+                    if(c==0)
+                    name.setText(result.get(0));
+                    else if(c==1)
+                        symptoms.setText(result.get(0));
+                    else if(c==2)
+                        diagnosis.setText(result.get(0));
+                    else if(c==3)
+                        prescription.setText(result.get(0));
+                    else if(c==4)
+                        advice.setText(result.get(0));
+                    c++;
                 }
         }
     }
